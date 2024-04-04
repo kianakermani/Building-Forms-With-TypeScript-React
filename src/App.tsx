99014f26195c356029ec1d8188faa68f0c87cf1d
kiana <kianakermani2001@gmail.com>
2024-04-04 19:26:06 +0430

    added validation with zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
-import { FormEvent } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 import "./App.css";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  msg: z
+    .string()
+    .min(3, { message: "Your Description Should Be More Than 3 Characters !" }),
+  msg2: z
+    .number({
+      invalid_type_error: "Your Amount Is Requierd",
+    })
+    .min(1000, {
+      message: "Your Amount Should Be At Least 1000 Toman !",
+    }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 function App() {
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log("submitted");
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
     <>
-      <form action="" className="form" onSubmit={handleSubmit}>
+      <form action="" className="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3">
           <label className="form-label">Description</label>
-          <input type="text" className="form-control" />
+
+          <input type="text" className="form-control" {...register("msg")} />
+          {errors.msg && <p className="text-danger">{errors.msg.message}</p>}
         </div>
         <div className="mb-3">
           <label className="form-label">Amount</label>
-          <input type="text" className="form-control" />
+          <input
+            type="number"
+            className="form-control"
+            {...register("msg2", { valueAsNumber: true })}
+          />
+          {errors.msg2 && <p className="text-danger">{errors.msg2.message}</p>}
         </div>
         <div className="mb-3">
           <label htmlFor="Category" className="form-label">
