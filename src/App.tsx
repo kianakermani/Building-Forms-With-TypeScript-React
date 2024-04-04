ee08c5930a25d58b8c804b3297ab69555baf6060
kiana <kianakermani2001@gmail.com>
2024-04-04 17:43:44 +0430

    built the structure of html elements

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <form action="" className="form">
+        <div className="mb-3">
+          <label className="form-label">Description</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Amount</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Category" className="form-label">
+            Category
+          </label>
+          <select name="" id="" className="form-control">
+            <option value="Groceries">Groceries</option>
+            <option value="Utilities">Utilities</option>
+            <option value="Entertainment">Entertainment</option>
+          </select>
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+
+      <div className="drop">
+        <select name="" id="" className="form-control ">
+          <option value="All Categories">All Categories</option>
+          <option value="Groceries">Groceries</option>
+          <option value="Utilities">Utilities</option>
+          <option value="Entertainment">Entertainment</option>
+        </select>
+      </div>
+      <div className="t">
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">Description</th>
+              <th scope="col">Amount</th>
+              <th scope="col">Category</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default App;
